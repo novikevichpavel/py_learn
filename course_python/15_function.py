73971a7b92e822aa29f9ca397cac91a4c332858a
novikevichpavel <novikevichp@gmail.com>
2026-02-21 19:49:30 +0300

    Create new repository

---
+++ course_python/15_function.py
@@ -0,0 +1,26 @@
+# def my_fn(a, b):
+#     a = a + 1
+#     c = a + b
+#     return c
+
+# Встроенные в пайтон функции:
+# print()
+# len()
+# round()
+# int()
+# type()
+# sum()
+# min()
+# str()
+# id()
+# input()
+# max()
+# bool()
+# dir() - выводит все атрибуты того или объекта
+
+# Методы вызываются для объекта через точку name.upper()
+
+name = input("Enter ypur name: ")
+city = input("Enter your current city: ")
+print(f"Your name is {name}. Ypu live in {city}")
+print(name.capitalize())
---
+++ course_python/7_func.py
@@ -0,0 +1,11 @@
+# def hello(name):
+#     print("Hello there", name)
+
+# hello("Pavel")
+
+def sum_nums(a, b):
+    sum = a + b
+    print(sum)
+
+sum_nums(5, 5)
+sum_nums(10.5, 11.2)
