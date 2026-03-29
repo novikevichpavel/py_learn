f6a4e5311cab150f5086c1096bf24abaecebefaf
novikevichpavel <novikevichp@gmail.com>
2026-03-29 20:28:36 +0300

    Change homework

--- homeworks/files.py
+++ homeworks/files.py
@@ -1,23 +1,25 @@
 from pathlib import Path
 
-if not "files":
-    Path("Users") / "pavel" / "my_folder" / "python_learning_repository" / "files"
+new_folder = Path("/Users") / "pavel" / "my_folder" / "python_learning_repository" / "files"
 
-with open("first.txt", "w") as first:
+if not new_folder.exists():
+    new_folder.mkdir()
+
+with open(new_folder / "first.txt", "w") as first:
     first.write("First line of the first file\n")
     first.write("Second line of the first file\n")
     first.write("Third line of the first file\n")
 
-with open("second.txt", "w") as second:
+with open(new_folder / "second.txt", "w") as second:
     second.write("First line of the second file\n")
     second.write("Second line of the second file\n")
     second.write("Third line of the second file\n")
     
-with open("first.txt") as first:
+with open(new_folder / "first.txt") as first:
     for line in first:
         print(line)
 
-with open("second.txt") as second:
+with open(new_folder / "second.txt") as second:
     while True:
         line = second.readline()
         print(line)
@@ -25,5 +27,11 @@ with open("second.txt") as second:
             break
 
 
+if Path(new_folder / "first.txt").exists():
+    Path(new_folder / "first.txt").unlink()
 
-    
+if Path(new_folder / "second.txt").exists():
+    Path(new_folder / "second.txt").unlink()
+
+if new_folder.exists():
+    new_folder.rmdir()
